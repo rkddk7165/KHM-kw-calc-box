6fcc462cb7639453cc247c76e76f7b9be62d2fc3
hyunmun kang <h2020202092@office.kw.ac.kr>
2023-11-01 19:23:24 +0900

    First Commit

---
+++ lib.js
@@ -0,0 +1,36 @@
+    
+    
+//average function
+function avg(numbers) {
+    let s = numbers.reduce((prev, curr) => prev + curr, 0);
+    return s / numbers.length;
+    }
+
+//prime function
+ function prime(num) {
+      // 1은 소수가 아니다.
+      if (num <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+  //factorial function
+  function fact(num) {
+    if (num === 0 || num === 1){
+        return 1;
+    }
+    return num * fact(num - 1);
+  }
+
+
+module.exports = {
+    avg,
+    prime,
+    fact
+  }
---
+++ lib.test.js
@@ -0,0 +1,29 @@
+const { test, expect } = require("@jest/globals");
+const lib = require("./lib");
+
+// test avg()
+test("avg([3, 5, 7]) should be 5", () => {
+    expect(lib.avg([3, 5, 7])).toBe(5);
+});
+
+test("avg([-5, 5]) should be 0", () => {
+    expect(lib.avg([-5, 5])).toBe(0);
+});
+
+// test prime()
+test("prime(5) should be true", () => {
+    expect(lib.prime(5)).toBe(true);
+});
+
+test("prime(24) should be false", () => {
+    expect(lib.prime(24)).toBe(false);
+});
+
+// test fact()
+test("fact(5) should be 120", () => {
+    expect(lib.fact(5)).toBe(120);
+});
+
+test("fact(7) should be 5040", () => {
+    expect(lib.fact(7)).toBe(5040);
+});
